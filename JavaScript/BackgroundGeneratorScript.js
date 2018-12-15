35b3805b78d896695fc3c13f2690021ed0a256a6
Kritika Sharma <kritikasharma1996@rediff.com>
2018-12-15 17:48:44 -0500

    Bonus question done for BackgroundGenerator

--- JavaScript/BackgroundGeneratorScript.js
+++ JavaScript/BackgroundGeneratorScript.js
@@ -3,6 +3,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("randomColorButton"); 
 
 function setGradient(){
 	body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
@@ -16,4 +17,11 @@ color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
+button.addEventListener("click", function(){
+	var colour1 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	var colour2 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	body.style.background = "linear-gradient(to right, " + colour1 + "," + colour2 + ")";
+
+	css.textContent = body.style.background + ";";
+});
 
