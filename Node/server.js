0e9223f2c3f65c2c447c5b5866c5bb9579ecb571
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-17 14:34:22 -0500

    implementing get and post requests using express server

--- Node/server.js
+++ Node/server.js
@@ -2,7 +2,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World!</h1?");
+    res.send("Getting Root");
+});
+
+app.get('/profile', (req, res) => {
+    res.send("Getting Profile");
+});
+
+app.post('/', (req, res) => {
+    const user = {
+        name: 'KS',
+        hobby: 'Dancing'
+    }
+    res.send(user);
 });
 
 app.listen(3000);
