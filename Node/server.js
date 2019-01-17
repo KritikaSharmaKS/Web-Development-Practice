4c742e6e779867a64318dac805dbfb6d0d08dd33
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-17 13:57:25 -0500

    methods available to request

--- Node/server.js
+++ Node/server.js
@@ -1,6 +1,11 @@
 const http = require('http');
 
 const server = http.createServer( (request, response) => {
+    // console.log('headers: ', request.headers);
+    console.log('url: ', request.url);
+    console.log('method: ', request.method);
+
+
     response.setHeader('Content-Type', 'text/html');
     response.end('<h1>Hello World!</h1>');
 });
