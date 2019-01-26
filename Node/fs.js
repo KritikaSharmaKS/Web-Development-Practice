b7c9340607e17bdf5028e984e15f424bfcccccef
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-25 19:13:42 -0500

    fs module readFile implemented

--- Node/fs.js
+++ Node/fs.js
@@ -4,5 +4,8 @@ fs.readFile('./hello.txt', (err, data) => {
     if(err){
         console.log("errrrooooooorrrrrr!!!");
     }
-    console.log(data);
+    console.log('Async',data.toString());
 });
+
+const file = fs.readFileSync('./hello.txt');
+console.log('Sync', file.toString());
