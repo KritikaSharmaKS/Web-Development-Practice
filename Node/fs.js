8d58477ff28af2152e8bf14a73977aa1515aa39e
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-25 19:42:06 -0500

    fs module append, write, delete implemented

--- Node/fs.js
+++ Node/fs.js
@@ -9,3 +9,28 @@ fs.readFile('./hello.txt', (err, data) => {
 
 const file = fs.readFileSync('./hello.txt');
 console.log('Sync', file.toString());
+
+//Append
+fs.appendFile('./hello.txt', ' You are a web developer now:)' , err => {
+    if(err){
+        console.log(err)
+    }
+});
+
+
+//Write
+fs.writeFile('bye.txt', 'Sad to see you go', err => {
+    if(err){
+        console.log(err)
+    }
+});
+
+//Delete
+fs.unlink('bye.txt', err => {
+    if(err){
+        console.log(err)
+    }
+    console.log("It's Deleted");
+});
+
+
