14ede4cd2b9c5c6132f790086c838880f5521a11
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-26 15:15:37 -0500

    part 2 of advent of code coding challenge completed

--- Node/Santa Puzzle(AdventOfCode)/Solution.js
+++ Node/Santa Puzzle(AdventOfCode)/Solution.js
@@ -7,12 +7,18 @@ fs.readFile('input.txt', (err, data) => {
     }
     const charArray = data.toString().split("");
     let floor=0;
+    let count = 1;
     for(ch of charArray){
         if(ch === '('){
             floor+=1
         }else{
             floor-=1
         }
+        if(floor === -1){
+            console.log("position is: "+count)
+            break;
+        }
+        count++;
     }
     console.log("floor is: "+floor);
     console.timeEnd('funCode');
