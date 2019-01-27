bba52e6db14ed1d47cd339e70aaef4b4afccb1bc
KritikaSharmaKS <sh.kritika01@gmail.com>
2019-01-27 16:11:52 -0500

    more efficient way of doing the advent of code challenge

--- Node/Santa Puzzle(AdventOfCode)/Solution.js
+++ Node/Santa Puzzle(AdventOfCode)/Solution.js
@@ -6,20 +6,44 @@ fs.readFile('input.txt', (err, data) => {
         console.log("errrroorrrrr!!!");
     }
     const charArray = data.toString().split("");
-    let floor=0;
-    let count = 1;
-    for(ch of charArray){
-        if(ch === '('){
-            floor+=1
-        }else{
-            floor-=1
+    // let floor=0;
+    let count = 0;
+    // for(ch of charArray){
+    //     if(ch === '('){
+    //         floor+=1
+    //     }else{
+    //         floor-=1
+    //     }
+    //     if(floor === -1){
+    //         console.log("position is: "+count)
+    //         break;
+    //     }
+    //     count++;
+    // }
+
+    // const answer = charArray.reduce((accumulator, currentValue) => {
+    //     if(currentValue==='('){
+    //         return accumulator+=1;
+    //     }
+    //     else{
+    //         return accumulator-=1;
+    //     }
+    // }, 0);
+
+    let accumulator = 0;
+    const position = charArray.some((currentItem) => {
+        if(currentItem==='('){
+            accumulator+=1;
         }
-        if(floor === -1){
-            console.log("position is: "+count)
-            break;
+        else{
+            accumulator-=1;
         }
         count++;
-    }
-    console.log("floor is: "+floor);
+        return accumulator<0;
+    });
+
+    console.log(count);
+
+    // console.log("floor is: "+floor);
     console.timeEnd('funCode');
 });
